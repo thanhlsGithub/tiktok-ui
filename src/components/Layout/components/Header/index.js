b1b980baa567360619ed4199c552700e39ea0925
thanhlsGithub <thanhga977ls@gmail.com>
2022-06-10 17:04:33 +0700

    Bai 109. Sửa lỗi và hoàn thiện phần Header 2

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -38,7 +38,8 @@ const MENU_ITEMS = [
                     title: 'Vietnamese',
                     type: 'language',
 
-                }
+                },
+
 
             ],
         },
--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -50,11 +50,12 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
                             setHistory(prev => prev.slice(0, prev.length - 1))
                         }} />}
 
-                        {renderItems()}
+                        <div className={cx('menu-body')}> {renderItems()}</div>
                     </PopperWrapper>
                 </ div>
             )}
             onHide={() => setHistory((prev) => prev.slice(0, 1))}
+
         >
             {children}
 
